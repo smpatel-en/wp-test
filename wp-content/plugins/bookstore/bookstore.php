13d948f31e5f244df06a671a7bb7df3e86aa5f42
thesujalpatel <patelsujal24102004@gmail.com>
2025-12-01 14:52:24 +0530

    Created post type plugin

---
+++ wp-content/plugins/bookstore/bookstore.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Plugin Name: Bookstore
+ * Description: A plugin to manage books
+ * Version: 1.0
+ */
+
+if (!defined("ABSPATH")) {
+    exit();
+}
+
+add_action("init", "bookstore_register_book_post_type");
+
+function bookstore_register_book_post_type()
+{
+    $args = [
+        "lables" => [
+            "name" => "Books",
+            "singular_name" => "Book",
+            "mune_name" => "Books",
+            "add_new" => "Add New Book",
+            "add_new_item" => "Add New Book",
+            "new_item" => "New Book",
+            "edit_item" => "Edit Book",
+            "view_item" => "View Book",
+            "all_items" => "All Books",
+        ],
+        "public" => true,
+        "has_archive" => true,
+        "show_in_rest" => true,
+        "supports" => ["title", "editor", "author", "thumbnail", "excerpt"],
+    ];
+
+    register_post_type("book", $args);
+}
